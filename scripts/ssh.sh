5a2c2e5df595d19d6104766e227d6819d2c10ec3
Jianneng Li <jl982@cornell.edu>
2015-05-14 19:08:18 -0700

    explain port forwarding

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -1,3 +1,4 @@
-SSH_KEY=~/UCB/CAPSTONE/capstone.pem # replace this with the location of your SSH key
+SSH_KEY=capstone.pem # replace this with the location of your SSH key
 
+# Storm UI will be at http://localhost:9999/index.html, Ganglia will be at http://localhost:9999/ganglia/
 ssh -i $SSH_KEY -L 9999:storm00:8080 -L 9998:storm00:80 root@173.39.238.84 -p22228
